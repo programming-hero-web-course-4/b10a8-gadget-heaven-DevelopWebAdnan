26901337fda61741aab0d9264aaaf105f7e8b449
DevelopWebAdnan <adnaniqbal023@yahoo.com>
2025-05-21 00:56:15 +0600

    created and added JSON data; created state and loaded the fake data in the state

--- src/components/Gadgets/Gadgets.jsx
+++ src/components/Gadgets/Gadgets.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Gadgets = () => {
+
+    const [gadgets, setGadgets] = useState([]);
+
+    useEffect(() => {
+        fetch('./gadgetsData.json')
+        .then(res => res.json())
+        .then(data => setGadgets(data))
+
+    }, [])
     return (
         <div>
             <h2 className="text-4xl font-bold text-center">Gadgets</h2>
+            <p>{gadgets.length}</p>
         </div>
     );
 };
