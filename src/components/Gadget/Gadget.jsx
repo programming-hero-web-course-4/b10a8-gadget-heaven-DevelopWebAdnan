fe7cc95cf6028484b0f2bd6a4cf7c9df1dd23f9b
DevelopWebAdnan <adnaniqbal023@yahoo.com>
2025-05-25 17:15:04 +0600

    fixed padding and the images of the gadgets' cards

--- src/components/Gadget/Gadget.jsx
+++ src/components/Gadget/Gadget.jsx
@@ -5,17 +5,17 @@ const Gadget = ({ gadget }) => {
     const { product_title, product_image, price } = gadget;
 
     return (
-        <div className="card bg-base-100 w-96 shadow-sm">
-            <figure className="px-5 pt-5 h-44">
+        <div className="card bg-base-100 w-96 shadow-sm p-5">
+            <figure className="h-44 mb-4">
                 <img
                     src={product_image}
                     alt={product_title}
-                    className="rounded-xl" />
+                    className="rounded-xl h-full w-full" />
             </figure>
-            <div className="card-body">
-                <h2 className="card-title">{product_title}</h2>
+            <div className="">
+                <h2 className="card-title mb-4">{product_title}</h2>
                 <p>Price: {price}</p>
-                <div className="card-actions">
+                <div className="card-actions mt-4">
                     <button className="btn btn-primary">View Details</button>
                 </div>
             </div>
