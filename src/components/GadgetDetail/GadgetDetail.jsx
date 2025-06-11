239a3d201b632f3132b55e45c30b208dfc2f7ff5
DevelopWebAdnan <adnaniqbal023@yahoo.com>
2025-06-11 11:16:41 +0600

    provided fuctionality to the Add To Wish List button similar to the Add To Cart button

--- src/components/GadgetDetail/GadgetDetail.jsx
+++ src/components/GadgetDetail/GadgetDetail.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
 import Heading from '../Heading/Heading';
-import { addToStoredCart } from '../../utility/addToCart';
+import { addToStoredCart, addToStoredWishList } from '../../utility/addToCart';
 
 const GadgetDetail = () => {
 
@@ -16,6 +16,10 @@ const GadgetDetail = () => {
 
     const handleAddToCart = (id) => {
         addToStoredCart(id);
+    }
+
+    const handleAddToWishList = (id) => {
+        addToStoredWishList(id);
     }
 
 
@@ -62,7 +66,7 @@ const GadgetDetail = () => {
                                 Add To Card
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" className="size-[1.2em]"><path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" /></svg>
                             </button>
-                            <button className="btn btn-circle btn-lg">
+                            <button onClick={() => handleAddToWishList(product_id)} className="btn btn-circle btn-lg">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" className="size-[1.2em]"><path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" /></svg>
                             </button>
                         </div>
--- src/utility/addToCart.js
+++ src/utility/addToCart.js
@@ -23,4 +23,29 @@ const addToStoredCart = (id) => {
     }
 }
 
-export { addToStoredCart }
+const getStoredWishList = () => {
+    // wish-list
+    const storedWishListStr = localStorage.getItem('wish-list');
+    if(storedWishListStr) {
+        const storedWishList = JSON.parse(storedWishListStr);
+        return storedWishList;
+    }
+    else {
+        return [];
+    }
+}
+
+const addToStoredWishList = (id) => {
+    const storedWishList = getStoredWishList();
+    if(storedWishList.includes(id)) {
+        // already exists. Do not add it
+        console.log(id, 'already exists in the wish list');
+    }
+    else {
+        storedWishList.push(id);
+        const storedWishListStr = JSON.stringify(storedWishList);
+        localStorage.setItem('wish-list', storedWishListStr);
+    }
+}
+
+export { addToStoredCart, addToStoredWishList }
